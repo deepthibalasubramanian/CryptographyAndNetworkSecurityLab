726afce20646c945654b2133a5670dc9ca47cbef
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2024-01-09 15:14:10 +0530

    Accept depth from user

--- railFenceMethod.py
+++ railFenceMethod.py
@@ -1,7 +1,10 @@
 #rail fence method
 
 #get input from user
-plain_text = "Find Me If you Can"
+plain_text = "Find Me If you Can    "
 plain_text = "".join(plain_text.strip().split()).lower()
 print("You entered the following plaintext: "+plain_text)
 
+#accept depth from user
+depth = int(input("Enter depth value, m: "))
+print(f"You entered depth, m = {depth}")
