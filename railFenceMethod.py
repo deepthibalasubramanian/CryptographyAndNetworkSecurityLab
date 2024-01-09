7de15ec960e092af3bc0e430abfad0461e617d68
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2024-01-09 15:11:26 +0530

    Create railFenceMethod.py

---
+++ railFenceMethod.py
@@ -0,0 +1,7 @@
+#rail fence method
+
+#get input from user
+plain_text = "Find Me If you Can"
+plain_text = "".join(plain_text.strip().split()).lower()
+print("You entered the following plaintext: "+plain_text)
+
