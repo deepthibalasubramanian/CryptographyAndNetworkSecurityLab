fc812dc3243cdf880cb79a5a2dbe055862397111
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2024-02-06 15:34:37 +0530

    Update AESKeyGeneration.java

--- AESKeyGeneration.java
+++ AESKeyGeneration.java
@@ -1,15 +1,21 @@
 // AES key generation
 
-public class AESKeyGeneration{
+public class Main{
 
     public static void AESKeyGen(String plaintext) {
         // split into words, each of length 16 bytes
         String w1 = plaintext.substring(0,8);
+        String w2 = plaintext.substring(8,16);
+        String w3 = plaintext.substring(16,24);
+        String w4 = plaintext.substring(24);
         System.out.println(w1);
+        System.out.println(w2);
+        System.out.println(w3);
+        System.out.println(w4);
     }
 
 	public static void main(String[] args) {
-		String plaintext = "00112233445566778899AABBCDDEEFF";
+		String plaintext = "00112233445566778899AABBCCDDEEFF";
 		AESKeyGen(plaintext);
 	}
 }
